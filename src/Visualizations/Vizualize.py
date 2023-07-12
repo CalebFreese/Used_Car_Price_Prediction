52823304d2bfdb611b7ce55c0e42acd2e85fd13f
CalebFreese <calebfreese430@gmail.com>
2023-07-12 11:56:46 -0500

    exploratory

---
+++ src/Visualizations/Vizualize.py
@@ -0,0 +1,35 @@
+#scripts to create exploratory or results drive vizualizations
+
+import csv
+import scipy.stats as stats
+
+#get house values from interim data
+def house_values(file_name):
+
+    #output array
+    out_arr = []
+
+
+    with open(file_name, 'r') as file:
+        #creates csv reader
+        reader = csv.reader(file)
+
+        #iterate through rows
+        for row in reader:
+
+            #adds house value to array
+            out_arr.append(row[0])
+    
+    #returns output array
+    return out_arr
+
+
+#gets skewness
+def skew(arr):
+    skewness = stats.skew(arr)
+    print("Skewness: " + skewness)
+
+
+
+
+
