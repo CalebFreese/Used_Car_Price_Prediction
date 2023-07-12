1e5d1832b79a494dc81c661ec93217a33460417f
CalebFreese <calebfreese430@gmail.com>
2023-07-12 15:08:03 -0500

    Update

--- src/Data/Make_Dataset_Functions.py
+++ src/Data/Make_Dataset_Functions.py
@@ -1,6 +1,7 @@
 #This file will be used to store all fucntions to download or generate datasets
 
 import csv
+import numpy as np
 
 #function to read the raw dataset
 def read_dataset_raw(file_path):
@@ -37,9 +38,9 @@ def read_dataset_raw(file_path):
                 
                 elif(i == 12):
                     if(row[i] == 'furnished'):
-                        row[i] = 1
+                        row[i] = 2
                     elif(row[i] == 'semi-furnished'):
-                        row[i] = 0.5
+                        row[i] = 1
                     elif(row[i] == 'unfurnished'):
                         row[i] = 0
                 
@@ -64,4 +65,62 @@ def write_to_interim(arr, file_path):
 
         #writes rows to csv file
         writer.writerows(arr)
+
+#function to read and change interim data set
+def read_data_interim(file_path):
+
+    #initializes output array
+    out_arr = []
+
+    #opens file to read
+    with open(file_path, 'r') as file:
+
+        #creates csv reader
+        reader = csv.reader(file)
+
+        #iterate through row
+        for row in reader:
+
+            #creates a new_row to be appended
+            new_row = []
+            
+            #iterate through values in row
+            for i in range(len(row)):
+
+                #checks for outliers
+                if(int(row[3]) == 4):
+                    break
+
+                if(int(row[3] == 1) and int(row[0]) > 900000):
+                    break
+
+                if(int(row[3] == 2) and int(row[0]) > 1000000):
+                    break
+                
+                #ignore these attributes
+                if (i == 6 or i == 7 or i == 8):
+                    continue
+
+                new_row.append(row[i])
+
+            if(new_row == []):
+                continue
+            out_arr.append(new_row)
+    
+    #returns new output
+    return out_arr
+
+#writes to processed data
+def write_to_processed(arr, file_path):
+
+    #opens new file as csv file
+    with open(file_path, 'w', newline='') as file:
+
+        #creates csv writer
+        writer = csv.writer(file)
+
+        #writes rows to csv file
+        writer.writerows(arr)
+
+
 
--- src/Vizualizations/Vizualize.py
+++ src/Vizualizations/Vizualize.py
@@ -4,6 +4,10 @@ import csv
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+import pandas as pd
+import sys
+import os
 
 #get house values from interim data
 def house_values(file_name):
@@ -21,6 +25,53 @@ def house_values(file_name):
 
             #adds house value to array
             out_arr.append(int(row[0]))
+    
+    #returns output array
+    return out_arr
+
+def data_values(file_name):
+
+    #output array
+    out_arr = []
+
+
+    with open(file_name, 'r') as file:
+        #creates csv reader
+        reader = csv.reader(file)
+
+        #iterate through rows
+        for row in reader:
+
+            #temporary list for row
+            out_row = []
+
+            #iterate through row
+            for value in row:
+                #append integer value
+                out_row.append(int(value))
+            
+            #append row to array
+            out_arr.append(out_row)
+
+    
+    #returns output array
+    return out_arr
+
+#returns value of single attribute
+def attribute_values(file_name, attribute):
+    #output array
+    out_arr = []
+
+
+    with open(file_name, 'r') as file:
+        #creates csv reader
+        reader = csv.reader(file)
+
+        #iterate through rows
+        for row in reader:
+
+            #adds house value to array
+            out_arr.append(int(row[attribute]))
     
     #returns output array
     return out_arr
@@ -76,6 +127,93 @@ def plot_frequency(arr):
 
     # Display the plot
     plt.show()
+
+def correlation_heat_map(arr):
+
+    # Get the current working directory
+    current_directory = os.getcwd()
+
+    # Add the main folder to the sys.path list
+    main_folder_path = os.path.abspath(os.path.join(current_directory, '..'))
+    sys.path.append(main_folder_path)
+    
+    #Get File Path
+    file_path_read = os.path.join(current_directory, '..', 'Data', 'Raw', 'Housing.csv')
+
+    #make data frame
+    df = pd.DataFrame(arr)
+
+    #list of names for each item
+    name_row = []
+
+    #open raw data to get column names
+    with open(file_path_read, 'r') as file:
+        #creates csv reader
+        reader = csv.reader(file)
+
+        #count to check if first row
+        count = 0
+        for row in reader:
+            if(count == 0):
+                name_row = row
+                count += 1
+                break
+    
+    df.columns = name_row
+
+    # Compute the correlation matrix
+    correlation_matrix = df.corr()
+
+    # Create the heatmap
+    plt.figure(figsize=(10,10))  # Adjust the figure size as desired
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', square=True)
+
+    # Set the plot title
+    plt.title("Correlation Heatmap of Features")
+
+    # Display the plot
+    plt.show()
+
+#gives a scatter plot comparing an attribute to price
+def scatter(attribute_arr, price_arr):
+    # Create the scatterplot
+    plt.scatter(attribute_arr, price_arr)
+
+    # Set the y-axis tick format to display whole integers
+    plt.gca().get_yaxis().set_major_formatter(plt.FormatStrFormatter('%d'))
+
+    # Set the plot title and labels
+    plt.title("Scatterplot of Attribute vs Price")
+    plt.xlabel("Attribute")
+    plt.ylabel("Price")
+
+    # Display the plot
+    plt.show()
+
+#creates a box and whisker plot
+def box_whisker(attribute_arr, num_dif_attr, price_arr):
+
+    # Create an empty list for each number of bathrooms
+    data = [[] for _ in range(1, num_dif_attr)]
+
+    # Group the house prices based on the number of bathrooms
+    for price, bathrooms in zip(price_arr, attribute_arr):
+        data[bathrooms - 1].append(price)
+
+    # Create the box and whisker plot
+    plt.boxplot(data, labels=[str(i) + " bathrooms" for i in range(1, num_dif_attr)])
+
+    # Set the y-axis tick format to display whole integers
+    plt.gca().get_yaxis().set_major_formatter(plt.FormatStrFormatter('%d'))
+
+    # Set the plot title and labels
+    plt.title("House Prices by Number of Bathrooms")
+    plt.xlabel("Number of Bathrooms")
+    plt.ylabel("House Prices")
+
+    # Display the plot
+    plt.show()
+
 
 
 
